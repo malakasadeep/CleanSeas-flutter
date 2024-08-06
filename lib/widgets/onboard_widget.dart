cfbf4d609251bebe60bcb6746d12bfab16eef0bd
malakasadeep <151869451+malakasadeep@users.noreply.github.com>
2024-08-07 01:04:16 +0530

    feat: Update onboarding screen design and add Jua font

--- lib/widgets/onboard_widget.dart
+++ lib/widgets/onboard_widget.dart
@@ -1,4 +1,4 @@
-import 'package:clean_seas_flutter/constants/colours.dart';
+import 'package:clean_seas_flutter/screens/authentication/login_screen.dart';
 import 'package:flutter/material.dart';
 
 class IntroWidget extends StatelessWidget {
@@ -23,24 +23,34 @@ class IntroWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ColoredBox(
-      color: backgroundBlue,
-      //gradient: gradientbg,
-      child: Stack(
-        children: [
-          Container(
-            height: MediaQuery.of(context).size.height / 1.6,
-            decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage(image), fit: BoxFit.cover)),
-          ),
-          Positioned(
-            bottom: 0,
-            right: 0,
-            left: 0,
-            child: Container(
-              height: MediaQuery.of(context).size.height / 2.16,
+    return Container(
+      decoration: BoxDecoration(
+        gradient: const LinearGradient(colors: [
+          Color.fromARGB(255, 94, 196, 230),
+          Color.fromARGB(255, 35, 38, 133),
+        ]),
+      ),
+      child: ColoredBox(
+        color: Colors
+            .transparent, // Set to transparent since we're using a gradient
+        child: Stack(
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height / 1.6,
               decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(image),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: 0,
+              right: 0,
+              left: 0,
+              child: Container(
+                height: MediaQuery.of(context).size.height / 2.16,
+                decoration: BoxDecoration(
                   color: Colors.white,
                   borderRadius: BorderRadius.only(
                     topLeft: index == 0
@@ -49,86 +59,120 @@ class IntroWidget extends StatelessWidget {
                     topRight: index == 2
                         ? const Radius.circular(100)
                         : const Radius.circular(0),
-                  )),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 45),
-                child: Column(
-                  children: [
-                    const SizedBox(
-                      height: 62,
-                    ),
-                    AnimatedOpacity(
-                      duration: const Duration(milliseconds: 500),
-                      opacity: 1.0,
-                      child: Text(title,
+                  ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 45),
+                  child: Column(
+                    children: [
+                      const SizedBox(height: 62),
+                      AnimatedOpacity(
+                        duration: const Duration(milliseconds: 500),
+                        opacity: 1.0,
+                        child: Text(
+                          title,
                           style: const TextStyle(
-                              fontSize: 24, fontWeight: FontWeight.bold)),
-                    ),
-                    const SizedBox(
-                      height: 16,
-                    ),
-                    AnimatedOpacity(
-                      duration: const Duration(milliseconds: 500),
-                      opacity: 1.0,
-                      child: Text(
-                        description,
-                        style: const TextStyle(
-                            fontSize: 18, height: 1.5, color: Colors.grey),
-                        textAlign: TextAlign.center,
+                              fontSize: 24, fontWeight: FontWeight.bold),
+                        ),
                       ),
-                    )
-                  ],
+                      const SizedBox(height: 16),
+                      AnimatedOpacity(
+                        duration: const Duration(milliseconds: 500),
+                        opacity: 1.0,
+                        child: Text(
+                          description,
+                          style: const TextStyle(
+                              fontSize: 18, height: 1.5, color: Colors.grey),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-          Positioned(
-            bottom: 20,
-            right: 0,
-            left: 0,
-            child: Padding(
-              padding: const EdgeInsets.all(16),
-              child: skip
-                  ? Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        TextButton(
-                          onPressed: () {},
-                          child: const Text(
-                            'Skip Now',
-                            style: TextStyle(color: Colors.black),
+            Positioned(
+              bottom: 20,
+              right: 0,
+              left: 0,
+              child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: skip
+                    ? Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          TextButton(
+                            onPressed: () {},
+                            child: const Text(
+                              'Skip Now',
+                              style: TextStyle(color: Colors.black),
+                            ),
+                          ),
+                          GestureDetector(
+                            onTap: onTab,
+                            child: Container(
+                              padding: const EdgeInsets.all(16),
+                              decoration: BoxDecoration(
+                                gradient: LinearGradient(
+                                  colors: [
+                                    Color.fromARGB(255, 94, 196, 230),
+                                    Color.fromARGB(255, 35, 38, 133),
+                                  ],
+                                ),
+                                borderRadius: BorderRadius.circular(50),
+                              ),
+                              child: const Icon(Icons.arrow_circle_right,
+                                  color: Colors.white, size: 42),
+                            ),
+                          )
+                        ],
+                      )
+                    : SizedBox(
+                        height: 46,
+                        child: MaterialButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) {
+                                  return loginScreen(); // Add const here if loginScreen has a const constructor
+                                },
+                              ),
+                            );
+                          },
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8)),
+                          padding: EdgeInsets
+                              .zero, // No extra padding needed for gradient
+                          child: Ink(
+                            decoration: BoxDecoration(
+                              gradient: LinearGradient(
+                                colors: [
+                                  Color.fromARGB(255, 94, 196, 230),
+                                  Color.fromARGB(255, 35, 38, 133),
+                                ],
+                              ),
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            child: Container(
+                              alignment: Alignment.center,
+                              constraints: BoxConstraints(
+                                  minHeight: 46, minWidth: double.infinity),
+                              child: const Text(
+                                'Get Started',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 25,
+                                    fontWeight: FontWeight.w800),
+                              ),
+                            ),
                           ),
                         ),
-                        GestureDetector(
-                          onTap: onTab,
-                          child: Container(
-                            padding: const EdgeInsets.all(16),
-                            decoration: BoxDecoration(
-                                color: Colors.blueAccent,
-                                borderRadius: BorderRadius.circular(50)),
-                            child: const Icon(Icons.arrow_circle_right,
-                                color: Colors.white, size: 42),
-                          ),
-                        )
-                      ],
-                    )
-                  : SizedBox(
-                      height: 46,
-                      child: MaterialButton(
-                        color: Colors.blueAccent,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(8)),
-                        onPressed: () {},
-                        child: const Text('Get Started',
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 25,
-                                fontWeight: FontWeight.w800)),
                       ),
-                    ),
+              ),
             ),
-          )
-        ],
+          ],
+        ),
       ),
     );
   }
