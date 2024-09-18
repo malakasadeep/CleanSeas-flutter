3885f799bbe64e530ceac4f24dd5ff52354a1bde
Dewni Jayasuriya <dewnijayasuriya123@gmail.com>
2024-09-18 20:26:24 +0530

    set SingleChildScrollView for entire screen

--- lib/screens/seafoodGuide/seafood_guide_home.dart
+++ lib/screens/seafoodGuide/seafood_guide_home.dart
@@ -23,120 +23,176 @@ class SustainableSeafoodGuideHome extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Sustainable Seafood Guide Home'),
+        title: const Text('Sustainable Seafood Guide Home'),
         backgroundColor: const Color.fromARGB(255, 192, 237, 255),
         elevation: 0,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // Search Bar
-            TextField(
-              decoration: InputDecoration(
-                hintText: 'Search for Sea Creatures',
-                prefixIcon: Icon(Icons.search),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(8.0),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // Search Bar
+              TextField(
+                decoration: InputDecoration(
+                  hintText: 'Search for Sea Creatures',
+                  prefixIcon: const Icon(Icons.search),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
                 ),
               ),
-            ),
-            SizedBox(height: 20),
+              const SizedBox(height: 20),
 
-            // Explore Sea Creatures Section
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text('Explore Sea Creatures',
-                    style:
-                        TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-                TextButton(
-                  onPressed: () {},
-                  child: Text('See all'),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 150,
-              child: ListView(
-                scrollDirection: Axis.horizontal,
-                children: const [
-                  SeaCreatureCard(
-                      imageUrl: 'assets/images/starfish.png',
-                      name: 'Star Fish'),
-                  SeaCreatureCard(
-                      imageUrl: 'assets/images/urchin.png',
-                      name: 'The Sea Urchin'),
-                  SeaCreatureCard(
-                      imageUrl: 'assets/images/octopus.png',
-                      name: 'Blue Ring Octopus'),
-                  // Add more sea creatures as needed
+              // Explore Sea Creatures Section
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text('Explore Sea Creatures',
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text('See all'),
+                  ),
                 ],
               ),
-            ),
-            SizedBox(height: 20),
-
-            // Endangered Species Section
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text('Endangered Species',
-                    style:
-                        TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-                TextButton(
-                  onPressed: () {},
-                  child: Text('See all'),
+              SizedBox(
+                height: 150,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: const [
+                    SeaCreatureCard(
+                        imageUrl: 'assets/images/starfish.png',
+                        name: 'Star Fish'),
+                    SeaCreatureCard(
+                        imageUrl: 'assets/images/urchin.png',
+                        name: 'The Sea Urchin'),
+                    SeaCreatureCard(
+                        imageUrl: 'assets/images/octopus.png',
+                        name: 'Blue Ring Octopus'),
+                    // Add more sea creatures as needed
+                  ],
                 ),
-              ],
-            ),
-            SizedBox(
-              height: 150,
-              child: ListView(
-                scrollDirection: Axis.horizontal,
-                children: const [
-                  SeaCreatureCard(
-                      imageUrl: 'assets/images/turtle.png',
-                      name: 'Hawksbill Turtle'),
-                  SeaCreatureCard(
-                      imageUrl: 'assets/images/squid.png',
-                      name: 'Hawaiin Squid'),
-                  SeaCreatureCard(
-                      imageUrl: 'assets/images/whale.png', name: 'Whale Shark'),
-                  // Add more endangered species as needed
+              ),
+              const SizedBox(height: 20),
+
+              // Endangered Species Section
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text('Endangered Species',
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text('See all'),
+                  ),
                 ],
               ),
-            ),
-            SizedBox(height: 20),
+              SizedBox(
+                height: 150,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: const [
+                    SeaCreatureCard(
+                        imageUrl: 'assets/images/turtle.png',
+                        name: 'Hawksbill Turtle'),
+                    SeaCreatureCard(
+                        imageUrl: 'assets/images/squid.png',
+                        name: 'Hawaiin Squid'),
+                    SeaCreatureCard(
+                        imageUrl: 'assets/images/whale.png',
+                        name: 'Whale Shark'),
+                    // Add more endangered species as needed
+                  ],
+                ),
+              ),
+              const SizedBox(height: 20),
 
-            // Buttons for Recommendations and Sustainable Choices
-            ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                  foregroundColor: Colors.black,
-                  backgroundColor: Colors.grey[300]),
-              child: Text('Seasonal Recommendation'),
-            ),
-            SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                  foregroundColor: Colors.black,
-                  backgroundColor: Colors.grey[300]),
-              child: Text('Make Sustainable Choices'),
-            ),
-          ],
+              // Updated Buttons for Recommendations and Sustainable Choices
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.all(16.0),
+                  backgroundColor: Colors.grey[300],
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const [
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Seasonal Recommendation',
+                            style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(height: 4),
+                          Text(
+                            'Enjoy the best seafood options available throughout the year',
+                            maxLines: 2,
+                            softWrap: true,
+                            style: TextStyle(
+                              fontSize: 14,
+                              color: Colors.black54,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Icon(Icons.arrow_forward_ios),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 10),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.all(16.0),
+                  backgroundColor: Colors.grey[300],
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const [
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Make Sustainable Choices',
+                            style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(height: 4),
+                          Text(
+                            'Find out which seafood is good for consumption and which should be avoided',
+                            maxLines: 2,
+                            softWrap: true,
+                            style: TextStyle(
+                              fontSize: 14,
+                              color: Colors.black54,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Icon(Icons.arrow_forward_ios),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-      // bottomNavigationBar: BottomNavigationBar(
-      //   items: const [
-      //     BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
-      //     BottomNavigationBarItem(icon: Icon(Icons.search), label: ''),
-      //     BottomNavigationBarItem(icon: Icon(Icons.category), label: ''),
-      //     BottomNavigationBarItem(icon: Icon(Icons.delete), label: ''),
-      //     BottomNavigationBarItem(icon: Icon(Icons.info), label: ''),
-      //   ],
-      // ),
     );
   }
 }
@@ -145,22 +201,21 @@ class SeaCreatureCard extends StatelessWidget {
   final String imageUrl;
   final String name;
 
-  const SeaCreatureCard(
-      {super.key, required this.imageUrl, required this.name});
+  const SeaCreatureCard({super.key, required this.imageUrl, required this.name});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: 120,
-      margin: EdgeInsets.only(right: 10),
+      margin: const EdgeInsets.only(right: 10),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Expanded(
             child: Image.asset(imageUrl, fit: BoxFit.cover),
           ),
-          SizedBox(height: 5),
-          Text(name, style: TextStyle(fontSize: 14)),
+          const SizedBox(height: 5),
+          Text(name, style: const TextStyle(fontSize: 14)),
         ],
       ),
     );
